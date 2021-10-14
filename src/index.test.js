3c48c4524bda2cdf51cb96e574223a5a3cb82376
Vivienne Ansah <voa2000@hotmail.com>
2021-10-14 18:54:37 +0100

    Added projectType test

--- src/index.test.js
+++ src/index.test.js
@@ -4,4 +4,9 @@ describe('Starter project should', () => {
   it('output its name', () => {
     expect(projectName()).toEqual('Kata starter project');
   });
+
+  it('output its type', () => {
+    expect(projectType()).toEqual('Scrum');
+  });
+
 });
